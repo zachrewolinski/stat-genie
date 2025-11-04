c7105b892148e7b3890d047016e819f48b7ec846
zachrewolinski <zrewolinski@gmail.com>
2025-11-03 17:58:22 -0800

    update to example shell script

--- examples/analysis_and_eval/eval.sh
+++ examples/analysis_and_eval/eval.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH --partition=yugroup
 
 # NOTES: 
 #     1) RUN THIS SCRIPT FROM THE ROOT DIRECTORY OF THE REPO.
@@ -9,15 +10,14 @@
 #        (E.G., `export OPENAI_API_KEY="sk-..."`)
 #     3) This script assumes you have already run the analysis step.
 
-# run_eval_cmd="src/stat_genie/blade_pipeline/run_files/run_get_eval.py \
-# --multirun_load_path \
+# run_eval_cmd="blade/run_get_eval.py --multirun_load_path \
 # examples/analysis_and_eval/analysis_output/multirun_analyses.json --output_dir \
 # examples/analysis_and_eval/eval_output --llm_eval_config_path \
-# config/llm_eval_config.yml"
+# examples/analysis_and_eval/example_config.yml"
 
-run_eval_cmd="blade/run_get_eval.py --multirun_load_path \
+run_eval_cmd="src/stat_genie/blade_pipeline/run_files/run_get_eval.py --multirun_load_path \
 examples/analysis_and_eval/analysis_output/multirun_analyses.json --output_dir \
 examples/analysis_and_eval/eval_output --llm_eval_config_path \
-examples/analysis_and_eval/example_config.yml"
+examples/analysis_and_eval/gpt5_config.yml"
 
 poetry run python $run_eval_cmd
