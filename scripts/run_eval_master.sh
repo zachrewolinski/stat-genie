259b12b16d96c44e73f7a887aed54352b2228274
zachrewolinski <zrewolinski@gmail.com>
2026-01-22 14:10:09 -0800

    evals thus far

--- scripts/run_eval_master.sh
+++ scripts/run_eval_master.sh
@@ -5,7 +5,7 @@
 slurm_script="scripts/run_pairwise_eval.sh"
 
 # datasets=("mortgage" "panda_nuts" "reading" "soccer" "teachingratings")
-datasets=("affairs")
+datasets=("caschools" "crofoot" "hurricane" "amtl" "mortgage" "soccer" "boxes" "panda_nuts" "teachingratings")
 
 for dataset in "${datasets[@]}"; do
     echo "Submitting eval job for dataset: $dataset"
