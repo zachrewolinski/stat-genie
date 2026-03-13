070e142dd5c558f233a62ae729f7c0066ddc6d41
zachrewolinski <zrewolinski@gmail.com>
2026-03-13 09:07:00 -0700

    80% of the confidence experiments are complete, the rest will finish soon

--- agentic/confidence_experiments/scripts/confidence-runner.sh
+++ agentic/confidence_experiments/scripts/confidence-runner.sh
@@ -5,10 +5,7 @@
 # run from agentic/scalar_experiments/ (or sbatch from there so job cwd is scalar_experiments/).
 
 # list all blade datasets
-# datasets=("affairs" "amtl" "boxes" "caschools" "crofoot" "hurricane" "mortgage" "panda_nuts" "reading" "soccer" "teachingratings")
-# have already done "affairs" and "amtl" so skipping those for now
-# datasets=("boxes" "caschools" "crofoot" "hurricane" "mortgage" "panda_nuts" "reading" "soccer" "teachingratings")
-datasets=("affairs" "amtl")
+datasets=("affairs" "amtl" "boxes" "caschools" "crofoot" "hurricane" "mortgage" "panda_nuts" "reading" "soccer" "teachingratings")
 
 # list the distribution types
 distributions=("alt" "null")
@@ -17,19 +14,15 @@ distributions=("alt" "null")
 # pves=(0.0 0.5 1.0) # only used if distribution is "pve"
 
 # list all perturbation types
-perturbations=("anonymize") # "shuffle_names" "add_features" "positive_leading_statement" "negative_leading_statement")
+perturbations=("anonymize" "shuffle_names" "add_features" "positive_leading_statement" "negative_leading_statement")
 
 # number of runs per dataset-perturbation pair
-# num_runs=20
-num_runs=1
-
-# prompt version (1-4)
-# prompt_versions=(1 2 3 4)
+num_runs=20
 
 # analysis script name
 analysis_script="scripts/confidence.sh"
 
-# for each dataset-perturbation pair, run analysis.sh `num_runs` times
+# for each dataset-perturbation pair, run confidence.sh `num_runs` times
 for distribution in "${distributions[@]}"; do
     if [[ "$distribution" == "pve" ]]; then
         pve_list=("${pves[@]}")
@@ -37,6 +30,11 @@ for distribution in "${distributions[@]}"; do
         pve_list=("")
     fi
     for dataset in "${datasets[@]}"; do
+        # if the dataset is affairs and the distribution is alt,
+        # skip the run since we have already computed these confidences
+        if [[ "$dataset" == "affairs" ]] && [[ "$distribution" == "alt" ]]; then
+            continue
+        fi
         for perturbation in "${perturbations[@]}"; do
             for pve in "${pve_list[@]}"; do
                 for run_number in $(seq 1 $num_runs); do
